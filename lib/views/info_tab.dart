0c2bacec9358e2ecc1e91e8ca6e589de703adac2
leokna <leonardo_villanueva97@protonmail.com>
2024-08-11 20:46:58 -0600

    Archivo para la vista de informacion

---
+++ lib/views/info_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class InformacionTab extends StatelessWidget {
+  const InformacionTab({super.key});
+  /**TO-DO:
+   * Crear vista de información
+  **/
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text("InfoBae"),
+      ],
+    );
+  }
+}
