cde780eb3cdea44de260a0d76724e1799accbf44
leokna <leonardo_villanueva97@protonmail.com>
2024-08-11 20:56:46 -0600

    Vista principal de noticias

--- lib/views/noticias_tab.dart
+++ lib/views/noticias_tab.dart
@@ -1,17 +1,48 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../models/news_article.dart';
+import 'news_carousel.dart';
+import 'news_feed.dart';
+import '../services/data_loader.dart';
 
-class AppState with ChangeNotifier {
-  bool _isNews = false;
-  bool get isNews => _isNews;
+class NoticiasTab extends StatelessWidget {
+  const NoticiasTab({super.key});
 
-  String get contentTitle {
-    return _isNews
-        ? "Esta es la página de noticas"
-        : "Esta es la página de información";
-  }
-
-  void tooglePage(bool isNews) {
-    _isNews = isNews;
-    notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: loadAllNewsArticles(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final allArticles = snapshot.data!;
+            return FutureBuilder<List<NewsArticle>>(
+                future: loadAllNewsArticles(),
+                builder: (context, carouselSnapshot) {
+                  if (carouselSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (carouselSnapshot.hasError) {
+                    return Center(
+                        child: Text('Error: ${carouselSnapshot.error}'));
+                  } else {
+                    final carouselArticles = carouselSnapshot.data!;
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          NewsCarousel(articles: carouselArticles),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 50.0),
+                            child: NewsFeeds(articles: allArticles),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                });
+          }
+        });
   }
 }
